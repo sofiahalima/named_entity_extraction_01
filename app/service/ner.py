7775e78bf34abd0cd98e2f92091fde492f0a8925
Sofia <halima.sofia@gmail.com>
2025-06-19 15:42:27 +0800

    Update ner.py

--- app/service/ner.py
+++ app/service/ner.py
@@ -1,8 +1,10 @@
 import spacy
 import os
+import env
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 USE_BERT_NER = os.getenv("USE_BERT_NER", "true").lower() == "true"
+project_root = os.environ.get("PROJECT_ROOT")
 
 if USE_BERT_NER:
     tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
@@ -11,16 +13,17 @@ if USE_BERT_NER:
 
 
     def extract_entities(text):
-        doc = nlp(text)
+        result = nlp(text)
         return [{
-            "text": ent.text,
-            "label": ent.label_,
-            "start": ent.start_char,
-            "end": ent.end_char
-        } for ent in doc.ents]
+            "text": r["word"],
+            "label": r["entity_group"],
+            "start": r["start"],
+            "end": r["end"],
+            "score": r["score"]
+        } for r in result]
 else:
-
-    nlp = spacy.load('/Users/sofiahalima/PycharmProjects/EntityExtractionDemo/app/model/output/model-best')
+    model_dir = f"{project_root}/app/model/output/model-best'"
+    nlp = spacy.load(model_dir)
 
 
     def extract_entities(text):
