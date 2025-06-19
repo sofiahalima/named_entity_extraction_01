5bda25aed82d7e0d9da34673cf2694dce5b03646
Sofia <halima.sofia@gmail.com>
2025-06-19 15:43:58 +0800

    Update run_pipeline.py

--- app/service/run_pipeline.py
+++ app/service/run_pipeline.py
@@ -7,19 +7,14 @@ from app.service.ner import extract_entities
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
-load_dotenv(dotenv_path="/Users/sofiahalima/PycharmProjects/EntityExtractionDemo/app/.env")
-
 
 def run_pipeline():
     docs_array = load_doc_from_storage()
     for docs in docs_array:
         for doc in docs:  # assuming 'documents' is a list of dicts
             entities = extract_entities(doc["content"])
-            print(entities)
             persist_data_with_entities(doc,entities)
 
 
 if __name__ == "__main__":
-    load_dotenv(dotenv_path="/Users/sofiahalima/PycharmProjects/EntityExtractionDemo/app/.env")
     run_pipeline()
